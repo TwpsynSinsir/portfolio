16e3bf5ee46733a0c5e3d64994cd18225130ccea
Calum Wicks <calumwicks@gmail.com>
2025-05-03 00:37:58 +1200

    adding in knowledge component

---
+++ src/components/Knowledge.tsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import '../assets/styles/Knowledge.scss'
+import '/node_modules/flag-icons/css/flag-icons.min.css'
+
+function Knowledge() {
+  return (
+    <div className="container" id="knowledge">
+      <div className="items-container">
+        <h1>Knowledge</h1>
+        <div className="item-grid">
+          <div className="item">
+            <h2>Full Stack Web Development</h2>
+            <p>
+              I have built functional web applications from scratch using modern
+              technologies such as React and Rails. I am confident in both
+              frontend and backend development, with much experience working as
+              part of a team.
+            </p>
+
+            <div className="items-grid">
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg"
+                alt="javascript logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg"
+                alt="typescript logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg"
+                alt="react logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rails/rails-plain-wordmark.svg"
+                alt="rails logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+                alt="python logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+                alt="html5 logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+                alt="css3 logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+                alt="tailwind logo"
+              />
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg"
+                alt="sqlite logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg"
+                alt="postgresql logo"
+              />
+            </div>
+          </div>
+          <div className="item">
+            <h2>Tools of the Trade</h2>
+            <p>
+              To make the best use of my skills, I operate a wide range of tools
+              and programmes. Testing, CI/CD pipelines, and deployment tools are
+              vital parts of the process.
+            </p>
+            <div className="items-grid">
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg"
+                alt="vscode logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+                alt="git logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+                alt="github logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/oauth/oauth-original.svg"
+                alt="oauth logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg"
+                alt="docker logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"
+                alt="figma logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg"
+                alt="npm logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+                alt="node logo"
+              />
+
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"
+                alt="postman logo"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Knowledge
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,41 +8,38 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img
-            src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg"
-            alt="Avatar"
-          />
+          <img src="../assets/images/portrait.png" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
             <a
-              href="https://github.com/yujisatojr"
+              href="https://github.com/TwpsynSinsir"
               target="_blank"
               rel="noreferrer"
             >
               <GitHubIcon />
             </a>
             <a
-              href="https://www.linkedin.com/in/yujisato/"
+              href="https://www.linkedin.com/in/calum-wicks-468517218/"
               target="_blank"
               rel="noreferrer"
             >
               <LinkedInIcon />
             </a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Calum Wicks</h1>
+          <p>Full Stack Developer</p>
 
           <div className="mobile_social_icons">
             <a
-              href="https://github.com/yujisatojr"
+              href="https://github.com/TwpsynSinsir"
               target="_blank"
               rel="noreferrer"
             >
               <GitHubIcon />
             </a>
             <a
-              href="https://www.linkedin.com/in/yujisato/"
+              href="https://www.linkedin.com/in/calum-wicks-468517218/"
               target="_blank"
               rel="noreferrer"
             >
